d024eaa7ea3fbec51ba5819b9e2b0513c229871e
KevinhoKiss <kquissi@gmail.com>
2026-04-13 14:24:46 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -109,7 +109,8 @@ async def on_message(message):
     ]
 
     if any(p in texto for p in palavras_chave):
-        await message.reply(" O <#1479642544429076500> foi criado justamente para isso <:FBI:1466776866122629252>")
+        await message.reply(" O <#1479642544429076500> foi criado justamente para isso <:FBI:1466776866122629252>",
+    mention_author=False)
         print("✅ Resposta enviada")
     else:
         print("❌ Nenhuma palavra-chave encontrada")
